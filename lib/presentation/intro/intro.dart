c3bae1d11d1ee70b6d136443882cf6604d74b35a
erkebulanice <erkebulanice@gmail.com>
2022-03-27 06:08:52 +0600

    fix intro page

--- lib/presentation/intro/intro.dart
+++ lib/presentation/intro/intro.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:studentor_mobile/presentation/login/login.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({Key? key}) : super(key: key);
@@ -15,7 +14,6 @@ class IntroPage extends StatelessWidget {
             Image.asset('assets/images/intro_back.png'),
             Column(
               crossAxisAlignment: CrossAxisAlignment.center,
-              // mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Padding(
                   padding: const EdgeInsets.all(20.0),
